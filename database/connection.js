665645f7dc5f8d361a335669835ca0db813793f8
pranavraagz <pranavraagz@gmail.com>
2021-07-17 17:33:30 +0530

    Add persistence to DB

--- database/connection.js
+++ database/connection.js
@@ -1,5 +1,10 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("sqlite::memory");
+// const sequelize = new Sequelize("sqlite::memory");
+
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: 'database/database.sqlite'
+  });
 
 module.exports = sequelize;
